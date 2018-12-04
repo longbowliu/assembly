3e08b9c829903ddce3868b53b6912ad68982ef20
longbowliu <llb_feng@163.com>
2018-12-04 15:11:26 +0800

    add llb_car file

---
+++ llb_car.sh
@@ -0,0 +1,7 @@
+git clone https://github.com/longbowliu/llb_arduino_bridge.git
+git clone https://github.com/longbowliu/astra.git
+git clone https://github.com/longbowliu/LongbowRobot.git   #in file password  to launch
+git clone https://github.com/longbowliu/nav_indigo_llb_car.git
+git clone https://github.com/longbowliu/rbx1.git
+git clone https://github.com/longbowliu/rplidar_ros.git
+git clone https://github.com/longbowliu/assembly.git
