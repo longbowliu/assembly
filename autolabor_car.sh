5ef1ef3a193bf835b8dee0c7db4188c01fe261cb
longbowliu <llb_feng@163.com>
2018-12-04 13:55:31 +0800

    align

---
+++ autolabor_car.sh
@@ -0,0 +1,7 @@
+git clone https://github.com/longbowliu/al_arduino_bridge.git
+git clone https://github.com/longbowliu/astra.git
+git clone https://github.com/longbowliu/LongbowRobot.git
+git clone https://github.com/longbowliu/nav.git
+git clone https://github.com/longbowliu/rbx1.git
+git clone https://github.com/longbowliu/rplidar_ros.git
+git clone https://github.com/longbowliu/assembly.git
